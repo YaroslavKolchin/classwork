a36ecce206eeddfff494a44d4b4189fa60372c54
Ярослав Колчин <@local.itstep>
2019-02-13 18:07:35 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Model;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceUser serviceUser = new ServiceUser("https://randomuser.me/api/?results=1");
+            string result = serviceUser.GetJsonString();
+            Console.WriteLine(result);
+
+        }
+    }
+}
